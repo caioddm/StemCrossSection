763675aaf1343f803923a9afcd3343a6231453b3
caioddm <caioduarte.diniz@gmail.com>
2016-04-11 19:07:56 -0500

    Initial commit

---
+++ StemCrossSection/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace StemCrossSection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imboxDisplay = new Emgu.CV.UI.ImageBox();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.fbdImages = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.imboxDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imboxDisplay
+            // 
+            this.imboxDisplay.Location = new System.Drawing.Point(43, 374);
+            this.imboxDisplay.Name = "imboxDisplay";
+            this.imboxDisplay.Size = new System.Drawing.Size(1351, 197);
+            this.imboxDisplay.TabIndex = 2;
+            this.imboxDisplay.TabStop = false;
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFolder.Location = new System.Drawing.Point(40, 51);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(139, 26);
+            this.lblFolder.TabIndex = 3;
+            this.lblFolder.Text = "Select folder:";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(185, 57);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(312, 20);
+            this.txtFolder.TabIndex = 4;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(519, 57);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(148, 23);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "Browse ...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(673, 57);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(85, 23);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1453, 632);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.imboxDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imboxDisplay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imboxDisplay;
+        private System.Windows.Forms.Label lblFolder;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.FolderBrowserDialog fbdImages;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ StemCrossSection/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.IO;
+using Emgu.CV.CvEnum;
+
+namespace StemCrossSection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //imboxDisplay.Image = img1;
+            imboxDisplay.FunctionalMode = ImageBox.FunctionalModeOption.PanAndZoom;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {            
+            DialogResult result = fbdImages.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFolder.Text = fbdImages.SelectedPath;
+            }
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            String searchFolder = txtFolder.Text;
+            var filters = new String[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp" };
+            Image<Bgr, Byte> img1 = new Image<Bgr, Byte>(GetFilesFrom(searchFolder, filters, false)[0]);
+            IdentifyContours(img1);
+        }
+
+        public static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        {
+            List<String> filesFound = new List<String>();
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            foreach (var filter in filters)
+            {
+                filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+            }
+            return filesFound.ToArray();
+        }
+
+        public void IdentifyContours(Emgu.CV.Image<Bgr, Byte> colorImage)
+        {
+
+            colorImage = colorImage.Resize(0.5, Inter.Linear);
+            Image<Gray, Byte> Img_Source_Gray = colorImage.Convert<Gray, Byte>();
+            Image<Gray, Byte> mask = Img_Source_Gray.ThresholdBinary(new Gray(90), new Gray(255));
+            Image<Bgr, byte> Img_Result_Bgr = new Image<Bgr, byte>(Img_Source_Gray.Width, Img_Source_Gray.Height);
+            Img_Source_Gray = Img_Source_Gray.And(mask);
+            CvInvoke.CvtColor(Img_Source_Gray, Img_Result_Bgr, ColorConversion.Gray2Bgr);
+
+            int largest_contour_index = 0;
+            double largest_area = 0;
+
+            using (Mat hierachy = new Mat())
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                IOutputArray hirarchy;
+
+                CvInvoke.FindContours(mask, contours, hierachy, RetrType.Tree, ChainApproxMethod.ChainApproxNone);
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    MCvScalar color = new MCvScalar(0, 0, 255);
+
+                    double a = CvInvoke.ContourArea(contours[i], false);  //  Find the area of contour
+                    if (a > largest_area)
+                    {
+                        largest_area = a;
+                        largest_contour_index = i;                //Store the index of largest contour
+                    }
+
+                    CvInvoke.DrawContours(mask, contours, largest_contour_index, new MCvScalar(255, 0, 0), 10);
+                }                
+            }
+
+
+            
+            //mask = mask.Erode(1);
+            mask = mask.Dilate(1);
+            mask = mask.Erode(1);
+            /*
+            Gray cannyThreshold = new Gray(100);
+            Gray circleAccumulatorThreshold = new Gray(120);
+            double Resolution = 2;
+            double MinDistance = 20;//colorImage.Width/30;
+            int MinRadius = 60;//colorImage.Width/30;
+            int MaxRadius = 200;//colorImage.Width/16;
+            CircleF[] HoughCircles = mask.Clone().HoughCircles(
+                                    cannyThreshold,
+                                    circleAccumulatorThreshold,
+                                    Resolution, //Resolution of the accumulator used to detect centers of the circles
+                                    MinDistance, //min distance 
+                                    MinRadius, //min radius
+                                    MaxRadius //max radius
+                                    )[0]; //Get the circles from the first channel
+            #region draw circles
+            foreach (CircleF circle in HoughCircles)
+                Img_Result_Bgr.Draw(circle, new Bgr(Color.Red), 2);
+            #endregion
+            */
+            imboxDisplay.Image = Img_Result_Bgr;
+        }
+    }
+}
